ffe59e72dd2e96860ec7f6968373786c840bd0e7
Charles Severance <csev@umich.edu>
2016-04-02 10:20:10 -0400

    Real initial commit

---
+++ j-test.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "jsonld.php";
+
+$x = file_get_contents('product.json');
+$result = array();
+for($i=0;$i<1000;$i++) {
+   $y = json_decode($x);
+   // $y = jsonld_compact($y, "http://schema.org/");
+    $result[] = $y;
+}
